e7ab7177af76a34e83def4a2a0a8ede79e5f540c
Carter Walsh <Cwalsh616@gmail.com>
2021-06-23 14:16:33 -0500

    code cleanup

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-
 const message = document.querySelector("#message");
 
 function addMovie(event) {
     event.preventDefault();
 
-    const inputField = document.querySelector("input").value;
     const movie = document.createElement("li");
     const movieTitle = document.createElement("span");
-    movieTitle.textContent = inputField;
-    movieTitle.addEventListener("click", crossOffMovie);
-    movie.appendChild(movieTitle);
-
-    document.querySelector("ul").appendChild(movie);
     const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "X";
-    deleteBtn.addEventListener("click", deleteMovie);
-    movie.appendChild(deleteBtn);
-    
+
+    const inputField = document.querySelector("input").value;
+    document.querySelector("ul").appendChild(movie);
     document.querySelector("input").value = "";
+
+    movieTitle.addEventListener("click", crossOffMovie);
+    deleteBtn.addEventListener("click", deleteMovie);
+    
+    movie.appendChild(movieTitle);
+    movie.appendChild(deleteBtn);
+
+    deleteBtn.textContent = "X";
+    movieTitle.textContent = inputField;
 }
 
 function deleteMovie(event) {
